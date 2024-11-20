d2b19b727338ae5490db0eaeaea32298f3ba212e
Sheviakov Denis <saivrem@gmail.com>
2024-11-20 14:36:25 +0200

    Attempt to fix SQL execution for tests

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Wiping docker containers and volumes"
+docker compose down
 docker rm -f icedata-v2-db && echo "DB Removed"
 docker volume rm live-demo_icedata-v2-mysql-volume && echo "DB Volume removed"
 docker rm -f icedata-v2-java && echo "Java app container removed"
