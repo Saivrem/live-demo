a66676e15d58d8c43ffb579a5167506efeb2998b
Sheviakov Denis <saivrem@gmail.com>
2024-11-22 15:58:49 +0200

    Refactor MySqlContainer Add gradle test logging

--- icedata-v2/src/test/java/biz/icecat/icedatav2/utils/containers/mysql/MySqlContainer.java
+++ icedata-v2/src/test/java/biz/icecat/icedatav2/utils/containers/mysql/MySqlContainer.java
@@ -1,18 +1,12 @@
 package biz.icecat.icedatav2.utils.containers.mysql;
 
-import biz.icecat.icedatav2.utils.containers.TestDockerContainer;
 import biz.icecat.icedatav2.utils.TestUtils;
-import com.github.dockerjava.api.command.ExecCreateCmdResponse;
+import biz.icecat.icedatav2.utils.containers.TestDockerContainer;
 import com.github.dockerjava.api.command.InspectContainerResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
-import org.testcontainers.shaded.com.github.dockerjava.core.command.ExecStartResultCallback;
 import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.ArrayList;
@@ -21,10 +15,16 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static biz.icecat.icedatav2.utils.TestUtils.isBuildRunningInDockerContainer;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 @Slf4j
 public class MySqlContainer extends TestDockerContainer<MySqlContainer> {
+    private static final String ROOT = "root";
+    private static final String RETRY_PATTERN = "Retry %s/%s.";
+    private static final int AWAIT_TIMEOUT = 120;
+    private static final int POLL_INTERVAL = 2;
+    private static final int MAX_RETRIES = AWAIT_TIMEOUT / POLL_INTERVAL;
 
     private final int port;
     private final String dbName;
@@ -35,46 +35,26 @@ public class MySqlContainer extends TestDockerContainer<MySqlContainer> {
     }
 
     private MySqlContainer(Class<?> testClass) {
-        // TODO newer version of mysql seem to drop native password, figure better approach later
         super(testClass, "mysql:8.0.24", "mysql-");
-        this.port = TestUtils.randomPort();
-        withFixedExposedPort(this.port, 3306);
-        withCreateContainerCmdModifier(cmd -> cmd.withAttachStderr(true).withAttachStdout(true));
+        port = TestUtils.randomPort();
         dbName = RandomStringUtils.randomAlphabetic(10).toLowerCase();
-        withEnv("MYSQL_DATABASE", dbName);
-        withEnv("MYSQL_ALLOW_EMPTY_PASSWORD", "true");
+        this.withFixedExposedPort(port, 3306)
+                .withCreateContainerCmdModifier(cmd -> cmd.withAttachStderr(true).withAttachStdout(true))
+                .withEnv("MYSQL_DATABASE", dbName)
+                .withEnv("MYSQL_ALLOW_EMPTY_PASSWORD", "true");
     }
 
     @Override
     protected void containerIsStarted(InspectContainerResponse containerInfo) {
         super.containerIsStarted(containerInfo);
         long currentTime = System.currentTimeMillis();
-        await().pollInterval(2, TimeUnit.SECONDS).atMost(120, TimeUnit.SECONDS).until(() -> {
-            ExecCreateCmdResponse execCommand = dockerClient.execCreateCmd(getContainerId())
-                    .withAttachStdout(true)
-                    .withCmd("mysqladmin", "-h", "localhost")
-                    .exec();
-            try (OutputStream stdout = new ByteArrayOutputStream();
-                 OutputStream stderr = new ByteArrayOutputStream()) {
-                dockerClient.execStartCmd(execCommand.getId())
-                        .exec(new ExecStartResultCallback(stdout, stderr))
-                        .awaitStarted()
-                        .awaitCompletion();
-                if (stdout.toString().contains("mysqld is alive")) {
-                    return true;
-                }
-            } catch (Exception e) {
-                log.error("Error executing isReady", e);
-            }
-            return false;
-        });
+        await().pollInterval(POLL_INTERVAL, TimeUnit.SECONDS)
+                .atMost(AWAIT_TIMEOUT, TimeUnit.SECONDS)
+                .until(() -> {
+                    ExecResult ping = execInContainer("mysqladmin", "-h", "localhost");
+                    return ping.getStdout().contains("mysqld is alive");
+                });
         log.info("MySQL is ready. Startup time: {}ms", (System.currentTimeMillis() - currentTime));
-    }
-
-    public String getMySqlConnectionString() {
-        String host = isBuildRunningInDockerContainer() ? getContainerName() : "127.0.0.1";
-        int port = isBuildRunningInDockerContainer() ? 3306 : this.port;
-        return String.format("jdbc:mysql://%s/%s", host + ":" + port, dbName);
     }
 
     public void createUser(String username, String password) {
@@ -89,95 +69,46 @@ public class MySqlContainer extends TestDockerContainer<MySqlContainer> {
         }
     }
 
-    private boolean userDoesNotExist(String username) {
-        if (createdUsers.contains(username)) {
-            log.debug("MySQL user {} already exists.", username);
-            return false;
-        }
-        return true;
-    }
-
     private void createSingleUser(String username, String password) {
-        await().pollInterval(2, TimeUnit.SECONDS).atMost(120, TimeUnit.SECONDS).until(() -> {
-            try (OutputStream createOut = new ByteArrayOutputStream(); OutputStream grantOut = new ByteArrayOutputStream()) {
-                String create = String.format("CREATE USER IF NOT EXISTS '%s'@'%%' IDENTIFIED BY '%s';", username, password);
-                ExecCreateCmdResponse createCommand = dockerClient.execCreateCmd(getContainerId())
-                        .withAttachStdout(true)
-                        .withCmd("mysql", "-uroot", "-e", create)
-                        .exec();
+        await().pollInterval(POLL_INTERVAL, TimeUnit.SECONDS)
+                .atMost(AWAIT_TIMEOUT, TimeUnit.SECONDS)
+                .until(() -> {
+                    String userQuery = createUserQuery(username, password);
+                    String grantQuery = grantQuery(username);
 
-                dockerClient.execStartCmd(createCommand.getId())
-                        .exec(new ExecStartResultCallback(createOut, null))
-                        .awaitStarted()
-                        .awaitCompletion();
-                if (StringUtils.isBlank(createOut.toString())) {
-                    log.info("MySQL user '{}' has been created.", username);
-                    String grant = String.format("GRANT ALL PRIVILEGES ON *.* TO '%s'@'%%';", username);
-                    ExecCreateCmdResponse grantCommand = dockerClient.execCreateCmd(getContainerId())
-                            .withAttachStdout(true)
-                            .withCmd("mysql", "-uroot", "-e", grant)
-                            .exec();
-                    dockerClient.execStartCmd(grantCommand.getId())
-                            .exec(new ExecStartResultCallback(grantOut, null))
-                            .awaitStarted()
-                            .awaitCompletion();
-                    if (StringUtils.isBlank(grantOut.toString())) {
-                        return true;
-                    } else {
-                        throw new IllegalStateException("Failed to grant permissions to MySQL user. Output: " + grantOut);
+                    ExecResult execResult = execInContainer(buildQuery(ROOT, null, userQuery));
+
+                    if (isBlank(execResult.getStderr())) {
+                        log.info("MySQL user '{}' has been created.", username);
+                        execResult = execInContainer(buildQuery(ROOT, null, grantQuery));
+
+                        if (isBlank(execResult.getStderr())) {
+                            log.info("Permissions granted to {}", username);
+                            return true;
+                        }
                     }
-                } else {
-                    throw new IllegalStateException("Failed to create MySQL user. Output: " + createOut);
-                }
-            } catch (IOException | InterruptedException e) {
-                throw new IllegalStateException("Failed to create MySQL user. Reason: " + e.getMessage(), e);
-            }
-        });
+
+                    return false;
+                });
     }
 
     private void checkConnectionReadiness(String username, String password) {
         AtomicInteger retryCount = new AtomicInteger(0);
-        await().pollInterval(2, TimeUnit.SECONDS).atMost(120, TimeUnit.SECONDS).until(() -> {
-            int currentRetryCount = retryCount.get();
-            String retryInfo = currentRetryCount == 0 ? "" : String.format("Retry %s/%s.", currentRetryCount, currentRetryCount);
-            log.debug("Testing database connection for user {}. {}", username, retryInfo);
-            ExecCreateCmdResponse execCommand =
-                    dockerClient.execCreateCmd(getContainerId())
-                            .withAttachStdout(true)
-                            .withAttachStderr(true)
-                            .withCmd("mysql", "-u", username, String.format("-p%s", password), "-e", "SHOW DATABASES")
-                            .exec();
-            try (OutputStream stdout = new ByteArrayOutputStream();
-                 OutputStream stderr = new ByteArrayOutputStream()) {
-                dockerClient.execStartCmd(execCommand.getId())
-                        .exec(new ExecStartResultCallback(stdout, stderr))
-                        .awaitStarted()
-                        .awaitCompletion();
-                String stdoutString = stdout.toString();
-                log.debug(stdoutString);
-                log.debug(stderr.toString());
-                if (stdoutString.contains(dbName)) {
-                    try (Connection ignored = DriverManager.getConnection(getMySqlConnectionString(), username, password)) {
-                        log.info("MySQL database {} is accepting connections from user {}.", dbName, username);
-                        return true;
-                    } catch (Exception ignored) {
-                    }
-                } else {
-                    print(stdoutString, stderr.toString());
-                }
-                retryCount.incrementAndGet();
-                return false;
-            }
-        });
-    }
+        await().pollInterval(POLL_INTERVAL, TimeUnit.SECONDS)
+                .atMost(AWAIT_TIMEOUT, TimeUnit.SECONDS)
+                .until(() -> {
+                    log.info(
+                            "Testing database connection for user {}. {}",
+                            username,
+                            RETRY_PATTERN.formatted(retryCount.getAndIncrement(), MAX_RETRIES)
+                    );
 
-    private void print(String stdoutString, String stderrString) {
-        if (StringUtils.isNotBlank(stdoutString)) {
-            log.debug("--- {}", stdoutString);
-        }
-        if (StringUtils.isNotBlank(stderrString)) {
-            log.debug("--- {}", stderrString);
-        }
+                    ExecResult showDatabases = execInContainer(buildQuery(username, password, showDatabasesQuery()));
+                    log.debug("Exec Result OUT: {}", showDatabases.getStdout());
+                    log.debug("Exec Result ERR: {}", showDatabases.getStderr());
+                    return showDatabases.getStdout().contains(dbName) &&
+                            verifyConnection(username, password);
+                });
     }
 
     @Override
@@ -188,5 +119,54 @@ public class MySqlContainer extends TestDockerContainer<MySqlContainer> {
     @Override
     protected Class<MySqlContainer> selfClass() {
         return MySqlContainer.class;
+    }
+
+    public String getMySqlConnectionString() {
+        String host = isBuildRunningInDockerContainer() ? getContainerName() : "127.0.0.1";
+        int port = isBuildRunningInDockerContainer() ? 3306 : this.port;
+        return String.format("jdbc:mysql://%s/%s", host + ":" + port, dbName);
+    }
+
+    private boolean userDoesNotExist(String username) {
+        if (createdUsers.contains(username)) {
+            log.debug("MySQL user {} already exists.", username);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean verifyConnection(String username, String password) {
+        try (Connection connection = DriverManager.getConnection(getMySqlConnectionString(), username, password)) {
+            log.debug("MySQL database {} is accepting connections from user {}.", dbName, username);
+            return true;
+        } catch (Exception exception) {
+            log.debug(exception.getMessage());
+        }
+        return false;
+    }
+
+    private String[] buildQuery(String user, String pass, String query) {
+        List<String> result = new ArrayList<>();
+        result.add("mysql");
+        result.add("-u");
+        result.add(user);
+        if (pass != null) {
+            result.add("-p%s".formatted(pass));
+        }
+        result.add("-e");
+        result.add(query);
+        return result.toArray(String[]::new);
+    }
+
+    private static String createUserQuery(String user, String pass) {
+        return "CREATE USER IF NOT EXISTS '%s'@'%%' IDENTIFIED BY '%s';".formatted(user, pass);
+    }
+
+    private static String grantQuery(String user) {
+        return "GRANT ALL PRIVILEGES ON *.* TO '%s'@'%%';".formatted(user);
+    }
+
+    private static String showDatabasesQuery() {
+        return "SHOW DATABASES";
     }
 }
